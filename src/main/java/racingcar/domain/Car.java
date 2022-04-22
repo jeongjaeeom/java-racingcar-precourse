e4258b0fe85006682a2203ac5e551a7805e3b2bc
jeongjaeeom <jeongjae.eom@gmail.com>
2022-04-22 18:53:00 +0900

    feat($compile): 자동차 이동 로직 구현
    자동차 이동 로직 구현
    - 주어진 값이(0에서 9사이에 랜덤값) 4이상일 경우 전진, 3 이하일 경우 멈춤
    - 자동차 객체 position 멤버 변수 추가
    - 자동차 객체 move() 메서드 구현
    
    BREAKING CHANGE: 자동차 이동 로직 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,8 +2,10 @@ package racingcar.domain;
 
 public class Car {
 
-    private final String name;
+    public static final int BOUNDARY_NUMBER = 4;
 
+    private final String name;
+    private int position;
     public Car(String name) {
         if (name == null || name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차명은 5글자를 초과할 수 없습니다.");
@@ -11,4 +13,14 @@ public class Car {
         this.name = name;
     }
 
+    public void move(int number) {
+        if (number < BOUNDARY_NUMBER) {
+            return;
+        }
+        this.position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,5 +13,21 @@ public class CarTest {
     void givenCarName_shouldNotEmptyAndFiveWord() {
         assertThatThrownBy(() -> new Car("내차는다섯글자가넘어요"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 전진")
+    void givenNumber_shouldMoveForward() {
+        Car car = new Car("tesla");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 멈춤")
+    void givenNumber_shouldStop() {
+        Car car = new Car("tesla");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
